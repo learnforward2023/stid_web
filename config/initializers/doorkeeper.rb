7fe092855c4b9e5bdb8ca1f8533b1141b08d4c92
Learn Forward 2023 <learnforward2023@gmail.com>
2024-07-26 15:24:50 +0900

    Update scopes

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -240,8 +240,8 @@ Doorkeeper.configure do
   # For more information go to
   # https://doorkeeper.gitbook.io/guides/ruby-on-rails/scopes
   #
-  default_scopes :public
-  # optional_scopes :write, :update
+  default_scopes(*%i[])
+  optional_scopes :openid, :profile, :email
 
   # Allows to restrict only certain scopes for grant_type.
   # By default, all the scopes will be available for all the grant types.
